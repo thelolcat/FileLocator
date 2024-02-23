4806bdd67b61d3731a04a0531f1f80fc81128c63
lolcat <105110312+thelolcat@users.noreply.github.com>
2024-02-23 07:43:58 +0530

    Add files via upload

---
+++ filelocator.py
@@ -0,0 +1,30 @@
+import os
+
+text = ""
+current_dir = os.path.abspath(os.getcwd())
+files = os.listdir()
+dirlist = []
+
+while continue:
+        text = input("FileLocator >> ")
+        if text != "":
+                find()
+        else:
+                break
+
+def find():
+        files = os.listdir()
+        for file in files:
+                if file.contains(text):
+                        print(os.path.abspath(file))
+                if os.path.isdir(file):
+                        dirlist.append(os.path.abspath(file))
+        nextdir()
+
+def nextdir():
+        if dirlist.len() != 0:
+                os.chdir(dirlist.pop(0))
+                find(text)
+        else:
+                os.chdir(current_dir)
+                print("--------")
