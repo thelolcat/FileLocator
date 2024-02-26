7003e2101e4b54212d321450c9d659098ead93bf
lolcat <105110312+thelolcat@users.noreply.github.com>
2024-02-26 07:14:45 +0530

    Update filelocator.py

--- filelocator.py
+++ filelocator.py
@@ -9,25 +9,25 @@ dirlist = []
 loop = 1
 
 def find():
-        files = os.listdir()
-        for file in files:
-                if file.contains(text):
-                        print(os.path.abspath(file))
-                if os.path.isdir(file):
-                        dirlist.append(os.path.abspath(file))
-        nextdir()
+    files = os.listdir()
+    for file in files:
+        if file.contains(text):
+            print(os.path.abspath(file))
+        if os.path.isdir(file):
+            dirlist.append(os.path.abspath(file))
+    nextdir()
 
 def nextdir():
-        if dirlist.len() != 0:
-                os.chdir(dirlist.pop(0))
-                find(text)
-        else:
-                os.chdir(current_dir)
-                print("--------")
+    if dirlist.len() != 0:
+        os.chdir(dirlist.pop(0))
+        find(text)
+    else:
+        os.chdir(current_dir)
+        print("--------")
 
 while loop == 1:
-        text = input("FileLocator >> ")
-        if text != "":
-                find()
-        else:
-                break
+    text = input("FileLocator >> ")
+    if text != "":
+        find()
+    else:
+        break
